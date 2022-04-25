9fe08101d483954825fa4b74126f11368edd152a
flepceska <flepceska@gmail.com>
2022-04-25 18:44:58 +0200

    'added file system.properties'

--- src/main/java/com/example/lab222/web/rest/BookRestController.java
+++ src/main/java/com/example/lab222/web/rest/BookRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://gentle-lowlands-81044.herokuapp.com")
 @RequestMapping("/api/books")
 public class BookRestController {
 
